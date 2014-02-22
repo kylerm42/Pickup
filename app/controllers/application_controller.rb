7861e31bfed7052aaf951c8bf1d089825daa7315
App Academy Student <student@appacademy.io>
2014-02-22 15:54:43 -0800

    Added Addresses to Event Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    return nil unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
   end
 
   helper_method :current_user
